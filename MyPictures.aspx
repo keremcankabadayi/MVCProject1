9ad9a5c26f0bd90203563a81f5a5f54376f6fbd5
Kerem Can <keremcankabadayi@anadolu.edu.tr>
2017-10-13 00:03:53 +0300

    project all changed with switch and cases

--- MyPictures.aspx
+++ MyPictures.aspx
@@ -1,34 +1,62 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="myPictures.aspx.cs" Inherits="_myPictures" %>
+
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-
     <title></title>
 </head>
-<body>
+<body style="height: 159px">
     <form id="form1" runat="server">
     <div>
     
-<img id="imageToSwap" src="images/a.jpg" />
+        Select one of my pictures </div>
+        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Height="31px" Width="135px" AutoPostBack="True">
+            <asp:ListItem>Select a picture</asp:ListItem>
+            <asp:ListItem>a</asp:ListItem>
+            <asp:ListItem>b</asp:ListItem>
+            <asp:ListItem>c</asp:ListItem>
+            <asp:ListItem>d</asp:ListItem>
+            <asp:ListItem>e</asp:ListItem>
+            <asp:ListItem>f</asp:ListItem>
+        </asp:DropDownList>
+<br />
 
-<br/><br/>
+        <asp:Image ID="Image1" runat="server" Height="392px" ImageAlign="Right" Visible="False" Width="561px" />
+        <br />
+        <br />
 
-<select id="dlist" onchange="swapImage()">
-    <option value="images/a.jpg">a</option>
-<option value="images/b.jpg" >b</option>
-<option value="images/c.jpg">c</option>
-</select>
+        <asp:CheckBox ID="CheckBox1" runat="server" AutoPostBack="True" OnCheckedChanged="CheckBox1_CheckedChanged" Text="Add a border to the picture" Visible="False" />
+        <br />
 
-<script type="text/javascript">
-    function swapImage() {
-        var image = document.getElementById("imageToSwap");
-        var dropd = document.getElementById("dlist");
-        image.src = dropd.value;
-    };
-</script>
-    </div>
+        <br />
+<asp:Label ID="Label1" runat="server" Text="Select border color :" Visible="False"></asp:Label>
+        <br />
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged" Visible="False">
+            <asp:ListItem>brown</asp:ListItem>
+            <asp:ListItem>blue</asp:ListItem>
+            <asp:ListItem>aqua</asp:ListItem>
+            <asp:ListItem>red</asp:ListItem>
+            <asp:ListItem>green</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Select size of the border :" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged" Visible="False">
+            <asp:ListItem Value="a1">1 pixel</asp:ListItem>
+            <asp:ListItem Value="a2">2 pixels</asp:ListItem>
+            <asp:ListItem Value="a3">3 pixels</asp:ListItem>
+            <asp:ListItem Value="a4">4 pixels</asp:ListItem>
+            <asp:ListItem Value="a5">5 pixels</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+
+        <p>
+
+            <asp:Label ID="Label3" runat="server" Text="Label" Visible="False"></asp:Label>
+        </p>
     </form>
 </body>
 </html>
--- MyPictures.aspx.cs
+++ MyPictures.aspx.cs
@@ -5,10 +5,136 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class MyPictures : System.Web.UI.Page
+public partial class _myPictures : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (DropDownList1.SelectedIndex == 0)
+        {
+            Image1.Visible = false;
+            CheckBox1.Visible = false;
+            RadioButtonList1.Visible = false;
+            Label2.Visible = false;
+            Label1.Visible = false;
+            DropDownList2.Visible = false;
+            Label3.Visible = false;
+
+        }
+        if (DropDownList1.SelectedIndex == 1)
+        {
+            Image1.Visible = true;
+            CheckBox1.Visible = true;
+            Label3.Visible = true;
+            Image1.ImageUrl = "images/a.jpg";
+        }
+        if (DropDownList1.SelectedIndex == 2)
+        {
+            Image1.Visible = true;
+            CheckBox1.Visible = true;
+            Label3.Visible = true;
+            Image1.ImageUrl = "images/b.jpg";
+        }
+        if (DropDownList1.SelectedIndex == 3)
+        {
+            Image1.Visible = true;
+            CheckBox1.Visible = true;
+            Label3.Visible = true;
+            Image1.ImageUrl = "images/c.jpg";
+        }
+        if (DropDownList1.SelectedIndex == 4)
+        {
+            Image1.Visible = true;
+            CheckBox1.Visible = true;
+            Label3.Visible = true;
+            Image1.ImageUrl = "images/d.jpg";
+        }
+        if (DropDownList1.SelectedIndex == 5)
+        {
+            Image1.Visible = true;
+            CheckBox1.Visible = true;
+            Label3.Visible = true;
+            Image1.ImageUrl = "images/e.jpg";
+        }
+        if (DropDownList1.SelectedIndex == 6)
+        {
+            Image1.Visible = true;
+            CheckBox1.Visible = true;
+            Label3.Visible = true;
+            Image1.ImageUrl = "images/f.jpg";
+        }
+    }
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+        if (CheckBox1.Checked == true)
+        {
+            Label1.Visible = true;
+            Label2.Visible = true;
+            DropDownList2.Visible = true;
+            RadioButtonList1.Visible = true;
+        }
+        else
+        {
+            Image1.BorderWidth = 0;
+            Label1.Visible = false;
+            Label2.Visible = false;
+            DropDownList2.Visible = false;
+            RadioButtonList1.Visible = false;
+        }
+    }
+    protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (RadioButtonList1.SelectedIndex == 0)
+        {
+            Image1.BorderColor = System.Drawing.Color.Brown;
+        }
+        if (RadioButtonList1.SelectedIndex == 1)
+        {
+            Image1.BorderColor = System.Drawing.Color.Blue;
+        }
+        if (RadioButtonList1.SelectedIndex == 2)
+        {
+            Image1.BorderColor = System.Drawing.Color.Aqua;
+        }
+        if (RadioButtonList1.SelectedIndex == 3)
+        {
+            Image1.BorderColor = System.Drawing.Color.Red;
+        }
+        if (RadioButtonList1.SelectedIndex == 4)
+        {
+            Image1.BorderColor = System.Drawing.Color.Green;
+        }
+
+    }
+    protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string strr = DropDownList2.SelectedItem.Value;
+            switch (strr)
+            {
+                case "a1":
+                    Image1.BorderWidth = 1;
+                    break;
+
+                case "a2":
+                    Image1.BorderWidth = 2;
+                    break;
+
+                case "a3":
+                    Image1.BorderWidth = 3;
+                    break;
+
+                case "a4":
+                    Image1.BorderWidth = 4;
+                    break;
+
+                case "a5":
+                    Image1.BorderWidth = 5;
+                    break;
+
+            }
     }
 }
